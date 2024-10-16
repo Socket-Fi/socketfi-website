65451f8bb08c3d2c045428ada88b988c5d4eaf8c
TinkerPal <tinkerpi2023@gmail.com>
2024-10-16 09:34:11 +0300

    web update

--- src/components/QuestBanner.jsx
+++ src/components/QuestBanner.jsx
@@ -50,24 +50,24 @@ export default function QuestBanner() {
           </div>
 
           <div className="max-w-md mx-auto text-center lg:max-w-none lg:text-left">
-            <h2 className="text-3xl font-bold text-black sm:text-4xl xl:text-5xl font-pj">
-              Get a demo 💪 Build landing pages.
+            <h2 className="text-3xl font-bold text-black sm:text-4xl xl:text-4xl font-pj">
+              Join Active Quests and Airdrops
             </h2>
             <p className="mt-8 text-base font-normal leading-7 text-gray-700 font-pj">
-              Create custom landing pages with Rareblocks that converts more
-              visitors than any website. With lots of unique blocks, you can
-              easily build a page.
+              Participate in 100s of active quests, bounties, airdrops, and
+              community engagement events to earn on-chain loyalty points,
+              badges, rewards and more..
             </p>
 
             <form action="#" method="POST" className="mt-16">
               <label for="" className="text-lg font-bold text-gray-900 font-pj">
                 {" "}
-                Provide email address to get free demo{" "}
+                View and join active quests and bounties
               </label>
               <div className="relative max-w-lg mx-auto mt-5 lg:mx-0">
                 <div className="absolute -inset-2">
                   <div
-                    className="w-full h-full mx-auto rotate-180 opacity-30 blur-lg filter"
+                    className="w-full h-full mx-auto rotate-180 opacity-20 blur-lg filter"
                     style={{
                       background:
                         "linear-gradient(90deg, #44ff9a -0.55%, #44b0ff 22.86%, #8b44ff 48.36%, #ff6644 73.33%, #ebff70 99.34%)",
@@ -76,7 +76,99 @@ export default function QuestBanner() {
                 </div>
 
                 <div className="relative">
-                  <input
+                  <div className="flex flex-col items-center mt-2 lg:justify-start sm:justify-center sm:flex-row  sm:space-x-5">
+                    <a
+                      href="https://app.socket.fi/quests"
+                      title=""
+                      className="
+                        relative
+                        inline-flex
+                        items-center
+                        justify-center
+                        w-full
+                        px-8
+                        py-2
+                        text-base
+                        font-bold
+                        text-white
+                        transition-all
+                        duration-200
+                        bg-gray-900
+                        border-2 border-transparent
+                        sm:w-auto
+                        focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900
+                        font-pj
+                        hover:bg-opacity-90
+                        rounded-xl
+                    "
+                      target="_blank"
+                    >
+                      View & join Quests
+                    </a>
+
+                    <a
+                      href="https://docs.socket.fi/"
+                      title=""
+                      className="
+          
+                        inline-flex
+                        items-center
+                        justify-center
+                        w-48
+                        px-8
+                        py-2
+                        mt-5
+                        text-base
+                        font-bold
+                        text-gray-900
+                        transition-all
+                        duration-200
+                       gap-1
+                        sm:w-autojustify-center sm:mt-0
+                        rounded-xl
+                        font-pj
+                        
+                     
+                       
+                    "
+                      target="_blank"
+                    >
+                      Learn more
+                      <div className="">
+                        <svg
+                          className="h-5 w-auto"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g clip-path="url(#clip0_1077_9075)">
+                            <path
+                              d="M9.16699 14.8887L20.167 3.88867"
+                              stroke="black"
+                              stroke-linecap="round"
+                            />
+                            <path
+                              d="M13.4492 3.53516H20.5203V10.6062"
+                              stroke="black"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                            <path
+                              d="M17.5 13.5V16.26C17.5 17.4179 17.5 17.9968 17.2675 18.4359C17.0799 18.7902 16.7902 19.0799 16.4359 19.2675C15.9968 19.5 15.4179 19.5 14.26 19.5H7.74C6.58213 19.5 6.0032 19.5 5.56414 19.2675C5.20983 19.0799 4.92007 18.7902 4.73247 18.4359C4.5 17.9968 4.5 17.4179 4.5 16.26V9.74C4.5 8.58213 4.5 8.0032 4.73247 7.56414C4.92007 7.20983 5.20982 6.92007 5.56414 6.73247C6.0032 6.5 6.58213 6.5 7.74 6.5H11"
+                              stroke="black"
+                              stroke-linecap="round"
+                            />
+                          </g>
+                          <defs>
+                            <clipPath id="clip0_1077_9075">
+                              <rect width="24" height="24" fill="white" />
+                            </clipPath>
+                          </defs>
+                        </svg>
+                      </div>
+                    </a>
+                  </div>
+                  {/* <input
                     type="email"
                     name=""
                     id=""
@@ -110,7 +202,7 @@ export default function QuestBanner() {
                     >
                       Get started now
                     </button>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </form>
